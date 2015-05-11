649e4774b2c413b3a4203b474d469f9f4986637d
sushritcode <sushritcode@users.noreply.github.com>
2015-05-11 11:49:29 +0530

    using a general title for all the pagees

--- admin/application/views/admin_header.php
+++ admin/application/views/admin_header.php
@@ -9,7 +9,7 @@ $avatar_root_url = $base_url."assets/avatars/";
 	<head>
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 		<meta charset="utf-8" />
-		<title>Login Page - Ace Admin</title>
+		<title>Quadridge Technologies : Letmeet Admin</title>
 
 		<meta name="description" content="User login page" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
