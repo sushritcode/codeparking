fb9258fc80906a1383b8cd01eaeca39eec8fc731
sushritcode <sushritcode@users.noreply.github.com>
2015-05-21 02:39:32 +0530

    updated for contact edit , enable and disable

--- admin/application/models/contacts_model.php
+++ admin/application/models/contacts_model.php
@@ -6,6 +6,16 @@ class Contacts_model extends CI_Model
 	{
 		parent::__construct();
 	}
+	function loadDetails($contactId)
+	{
+
+		if (is_numeric($contactId))
+		{
+			$this->db->where(array('ContactId' => $contactId));
+			return $this->db->get('LMContactDetails')->row();
+		}
+		return null;
+	}
 	function getAll()
 	{
 
@@ -17,8 +27,53 @@ class Contacts_model extends CI_Model
 		$query = $this->db->get();
 		$recordSet['list'] = $query->result();
 		return $recordSet;
-			
+
 	}
+	function updateContact()
+	{	
+
+		$this->load->helper('my_extra_functions');
+		$formTableMap = form_table_map();
+		$formValues = $this->input->get(NULL,true);
+		$numEntries =  $this->db->where("ContactId ='".trim($this->input->get('contactID'))."'")->get('LMContactDetails')->num_rows();
+		$flag=3;
+		if( $numEntries == 0)
+			return $flag;
+		if(isset($formValues['formname']))
+		{
+			if($formValues['formname'] != "")
+			{
+
+				foreach($formValues as $key => $value):
+					if($key != "formname"):
+					if(isset($formTableMap[$formValues['formname']][$key]) && $value !=""):
+					$this->db->set( $formTableMap[$formValues['formname']][$key] , trim($value));
+				endif;
+				endif;
+				endforeach;
+				$this->db->where("ContactId = '".$this->input->get('contactID')."'");
+				$this->db->update('LMContactDetails');
+				$flag=4;
+				return $flag;
+			}
+		}
+		return $flag;
+	}
+	function enable()
+	{
+		$this->db->set( "ContactStatus" , '1');
+		$this->db->where("ContactId = '".$this->uri->segment(3)."'");
+		$this->db->update('LMContactDetails');
+
+	}
+	function disable()
+	{
+		$this->db->set( "ContactStatus" , '0');
+		$this->db->where("ContactId = '".$this->uri->segment(3)."'");
+		$this->db->update('LMContactDetails');
+
+	}
+
 	function insertContact()
 	{
 
