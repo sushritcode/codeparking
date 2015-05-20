946a532b5b1b22cd9319dff146692828a0628490
sushritcode <sushritcode@users.noreply.github.com>
2015-05-21 02:38:35 +0530

    updated for edit , enable and disable contacts

--- admin/application/controllers/contacts.php
+++ admin/application/controllers/contacts.php
@@ -6,8 +6,54 @@ class Contacts extends CI_Controller
 
 	function __construct()
 	{
-		$this->loginRequired = Array("index","add","save");
+		$this->loginRequired = Array("index","add","save","load","update","enable","disable");
 		parent::__construct();
+	}
+	function enable()
+	{
+		$this->load->model('Contacts_model');
+		$this->Contacts_model->enable();
+     	        redirect($this->config->item('base_url').'contacts/', 'refresh');
+
+	}
+	function disable()
+	{
+		$this->load->model('Contacts_model');
+		$this->Contacts_model->disable(0);	
+     	        redirect($this->config->item('base_url').'contacts', 'refresh');
+	}
+
+	function update()
+	{
+		$this->load->model('Contacts_model');
+		$data["resultUpdate"] = $this->Contacts_model->updateContact();
+		$arrMessage = local_messages($this->router->class);
+		echo $arrMessage[$data["resultUpdate"]];
+
+	}
+	function load($contactId)
+	{
+		
+		$data = array();
+		$data['base_url'] = $this->config->item('base_url');
+		
+		$this->load->model('Contacts_model');
+		$this->load->helper('my_extra_functions');
+
+		
+		$data['loaderUI'] = modal_dialog_loader();
+		$data['form_table_map'] = form_table_map();
+		$data['contacts_group'] = contacts_group();
+
+		$data["resultGetDetails"] = $this->Contacts_model->loadDetails($this->uri->segment(3));
+		
+		$this->load->view('admin_header', $data);
+		$this->load->view('admin_navigation', $data);
+		$this->load->view('contacts/edit', $data);
+		$this->load->view('admin_footer', $data);
+
+
+		
 	}
 	function index()
 	{
